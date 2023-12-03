85c2e6a51a202a2eb458e79399bfdbbe8e0a2432
Foat <Khisamov.foat@mail.ru>
2023-12-03 12:56:42 +1000

    Семинар 1 и домашнее задание 03.12.2023

--- src/Seminar1/Task04.java
+++ src/Seminar1/Task04.java
@@ -7,17 +7,22 @@ import java.util.Arrays;
 
 public class Task04 {
     public static void main(String[] args) {
-        int[] array = new int[]{3,2,2,3};
+        int[] array = new int[]{3,2,3,1,3,4,5,9,10,3,3,12,15,111};
+        int[] array1 = new int [array.length];
         int num = 3;
-        int[] arr = new int[array.length];
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] == num){
-                arr[array.length - i] = array[i];
-            }else {
-                arr[i] = array[i];
-                i++;
+        int j = 0;
+        System.out.printf("%s -> ", Arrays.toString(array) );
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != num ){
+                array1[j] = array[i];
+                j++;
+            }
+            for (int k = 0; k < array1.length; k++) {
+                if (array1[k] == 0){
+                    array1[k] = num;
+                }
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(array1));
     }
 }
