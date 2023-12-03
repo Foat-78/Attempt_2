3d7242da0d5f462fe6906a0958be874d788e8de0
Foat <Khisamov.foat@mail.ru>
2023-12-03 13:26:25 +1000

    Семинар 1 и домашнее задание 03.12.2023

--- src/Seminar1/TaskDZ1.java
+++ src/Seminar1/TaskDZ1.java
@@ -1,11 +1,11 @@
 package Seminar1;
 
-import java.util.Scanner;
-
 // Вычислить n-ое треугольного число(сумма чисел от 1 до n).
 // Пример:
 //        n = 4 -> 10
 //        n = 5 -> 15
+
+import java.util.Scanner;
 public class TaskDZ1 {
     public static void main(String[] args) {
         System.out.print("Введите число n: ");
