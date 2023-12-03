f2772aa556f37469b9235e2f427889938bbd1dfe
Foat <Khisamov.foat@mail.ru>
2023-12-03 11:32:42 +1000

    Семинар 1 и домашнее задание 03.12.2023

--- src/Seminar1/Task07.java
+++ src/Seminar1/Task07.java
@@ -5,5 +5,22 @@ package Seminar1;
 //Paul Green 25
 //Mike Black 23
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Task07 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("Task07.txt");
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            String date = String.format("Name | Surname | Age\n" +
+                                        "------------------\n" +
+                                        "Kate |  Smith  | 20\n" +
+                                        "Paul |  Green  | 25\n" +
+                                        "Mike |  Black  | 23\n");
+            fileWriter.write(date);
+            fileWriter.flush();
+
+        }
+    }
 }
