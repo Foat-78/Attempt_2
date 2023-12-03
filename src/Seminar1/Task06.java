2535c78f36882dc1c7350ffa0e0a09c3991da96f
Foat <Khisamov.foat@mail.ru>
2023-12-03 12:57:47 +1000

    Семинар 1 и домашнее задание 03.12.2023

--- src/Seminar1/Task06.java
+++ src/Seminar1/Task06.java
@@ -4,8 +4,19 @@ package Seminar1;
 // Пример 2: а = 2, b = -2, ответ: 0.25
 // Пример 3: а = 3, b = 0, ответ: 1
 
+import java.util.Scanner;
+
 public class Task06 {
     public static void main(String[] args) {
-
+        System.out.print("Введите число а: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.print("Введите число b: ");
+        int b = scanner.nextInt();
+//        System.out.println(a + " " + b);
+        for (int i = 0; i < b; i++) {
+            a *= a;
+        }
+        System.out.println(a);
     }
 }
