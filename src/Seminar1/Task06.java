2bfe6f68a42530cd607f88ed907ed38d53e6aa74
Foat <Khisamov.foat@mail.ru>
2023-12-05 18:47:44 +1000

    Семинар 2 и домашнее задание 05.12.2023

--- src/Seminar1/Task06.java
+++ src/Seminar1/Task06.java
@@ -1,22 +1,25 @@
 package Seminar1;
-// Реализовать функцию возведения числа а в степень b. a, b из Z. Сводя количество выполняемых действий к минимуму.
+
+// Реализовать функцию возведения числа а в степень b. Сводя количество выполняемых действий к минимуму.
 // Пример 1: а = 3, b = 2, ответ: 9
 // Пример 2: а = 2, b = -2, ответ: 0.25
 // Пример 3: а = 3, b = 0, ответ: 1
 
 import java.util.Scanner;
-
 public class Task06 {
     public static void main(String[] args) {
         System.out.print("Введите число а: ");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int x = scanner.nextInt();
         System.out.print("Введите число b: ");
-        int b = scanner.nextInt();
-//        System.out.println(a + " " + b);
-        for (int i = 0; i < b; i++) {
-            a *= a;
+        int n = scanner.nextInt();
+
+        double p = n < 0 ? 1.0 / x : x;
+        int m = n < 0 ? -n : n;
+        double res = 1.0;
+        while (m-- > 0) {
+            res *= p;
         }
-        System.out.println(a);
+        System.out.println(res);
     }
 }
