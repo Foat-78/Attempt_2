d3255e9eb5c34c68b2e6fca7775254f482987549
Foat <Khisamov.foat@mail.ru>
2023-12-07 15:14:37 +1000

    Семинар 2 и домашнее задание 07.12.2023

--- src/Seminar2/Task05_log/Task05.java
+++ src/Seminar2/Task05_log/Task05.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //1. Напишите метод, который вернет содержимое текущей папки в виде
@@ -14,6 +15,7 @@ import java.util.logging.Logger;
 //возникновения исключения, оно должно записаться в лог-файл.
 public class Task05 {
     private final static String PATH = ".";
+
 
     private static final Logger LOG = log.log(Task05.class.getName());
     public static void main(String[] args) {
@@ -27,13 +29,14 @@ public class Task05 {
             String[] data = getFileNamesInDir();
             for (String s: data) {
                 in.append(s).append("\n");
-        }
+            }
             System.out.println("File names in goal dir recorded!!!");
             if (isThrow()){
                 throw new IOException("Some king of mistake");
             }
-    } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+           LOG.log(Level.INFO, e.getLocalizedMessage());
+           e.printStackTrace();
         }
 
 
@@ -47,7 +50,25 @@ public class Task05 {
         return digit > 0;
     }
 
-
+    /*
+    По пункту 1 задания.
+    Есть метод getFileNamesInDir() который возвращает массив строк и вычитывает из нашей
+    текущей папки все названия файлов.
+    Папка File dir равно new File(PATH) или можно просто записать так File(".") где точка обозначает
+    текущая папка.
+    Доводим переменную String[] result которая пока длиной будет равен нулю. Далее заводим массив файлов
+    File[] files и просто он ен инициализирован.
+    Далее если dir - это директория то мы File массивов под названием files инициализируем
+    через API listFiles() -> если files != null т. е. проинициализировалась File[] files - не пустое
+    то результат перезаписываю т.е. result будет равен [files.length] количеству файлов лежащих
+    в папке -> идем циклом for по массиву files и говору на каждой итерации file = files[i] и
+    кладём в переменную имени name, имени файла file.getName().
+    Далее проверяем если папка является файлом то выводим System.out.println("\tFile - " + name),
+    иначе выводим, что директория  System.out.println("Dirictorry - " + name) -> result[i]
+    присваиваем имя name.
+    Далее если это не директория то выводим System.out.println(dir.getName() + " не является папка")
+    и возвращаем результат.
+     */
     public static String[] getFileNamesInDir(){
         File dir = new File(PATH);
         String[] result = new String[0];
