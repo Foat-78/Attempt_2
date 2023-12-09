651bc25d00952ab475251e5f18d3de4cc1ef2a4a
Foat <Khisamov.foat@mail.ru>
2023-12-09 21:22:17 +1000

    Семинар 3 и домашнее задание 09.12.2023

---
+++ src/Seminar3/TaskDZ3.java
@@ -0,0 +1,28 @@
+package Seminar3;
+//«адан целочисленный список ArrayList. Ќайти минимальное, максимальное и среднее из
+//этого списка.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TaskDZ3 {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{4, 2, 7, 5, 1, 3, 8, 6, 9};
+        analyzeNumbers(arr);
+    }
+
+    public static void analyzeNumbers(Integer[] array){
+        ArrayList<Integer> arr = new ArrayList<>();
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            arr.add(array[i]);
+            sum = sum + array[i];
+        }
+        arr.sort(Comparator.naturalOrder());
+        System.out.println(arr);
+        System.out.println("Minimum is " + Collections.min(arr));
+        System.out.println("Maximum is " + Collections.max(arr));
+        System.out.println("Average is = " + sum /array.length);
+    }
+}
