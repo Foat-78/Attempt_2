94b7788483507ff7e372ec8f99d402e487951449
Foat <Khisamov.foat@mail.ru>
2023-12-09 18:25:19 +1000

    Семинар 2 и домашнее задание 09.12.2023

---
+++ src/Seminar3/Task01.java
@@ -0,0 +1,43 @@
+package Seminar3;
+//Даны следующие строки, cравнить их с помощью == и
+//метода equals() класса Object
+//String s1 = "hello";
+//String s2 = "hello";
+//String s3 = s1;
+//String s4 = "h" + "e" + "l" + "l" + "o";
+//String s5 = new String("hello");
+//String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
+public class Task01 {
+    public static void main(String[] args) {
+
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = s1;
+        String s4 = "h" + "e" + "l" + "l" + "o";
+        String s5 = new String("hello");
+        String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
+
+        isEquals(s1, s2);
+        System.out.println();
+        isEquals(s1, s3);
+        System.out.println();
+        isEquals(s1, s4);
+        System.out.println();
+        isEquals(s1, s5);
+        System.out.println();
+        isEquals(s1, s6);
+    }
+    public static void isEquals(String str, String str1){
+
+        if (str == str1){
+            System.out.println("Равны как ссылки!");
+        } else if (str != str1){
+            System.out.println("Не равны как ссылки!");
+        }
+        if (str.equals(str1)) {
+            System.out.println("Равны как значения!");
+        } else if (!str.equals(str1)){
+                System.out.println("Не равны как значения!");
+        }
+    }
+}
---
+++ src/Seminar3/Task01_1.java
@@ -0,0 +1,26 @@
+package Seminar3;
+//Заполнить список десятью случайными числами.
+//Отсортировать список методом sort() и вывести его на
+//экран
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Task01_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Random rn = new Random();
+        sort(list, rn);
+    }
+    public static void sort(ArrayList lst, Random rnd){
+        for (int i = 0; i < 10; i++) {
+            lst.add(rnd.nextInt(1,10));
+        }
+        System.out.print(lst);
+        lst.sort(Comparator.naturalOrder());
+        System.out.println(" -> Отсортированный на увеличение: " + lst);
+//        lst.sort(Comparator.reverseOrder());
+//        System.out.println(" -> Отсортированный на уменьшение: " + lst);
+    }
+}
