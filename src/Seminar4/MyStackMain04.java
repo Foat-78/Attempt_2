62085125cc619d81a59c8e54784fc1aed2e8389f
Foat <Khisamov.foat@mail.ru>
2023-12-16 20:12:46 +1000

    Семинар 4. 16.12.2023

---
+++ src/Seminar4/MyStackMain04.java
@@ -0,0 +1,18 @@
+package Seminar4;
+
+public class MyStackMain04 {
+    public static void main(String[] args) {
+        MyStack04 myStack = new MyStack04();
+        System.out.println(myStack.size());
+        myStack.push(22);
+        myStack.push(25);
+        myStack.push(12);
+//        myStack.push(100);
+        System.out.println(myStack.pop());
+        System.out.println(myStack.peek());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.peek());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.peek());
+    }
+}
