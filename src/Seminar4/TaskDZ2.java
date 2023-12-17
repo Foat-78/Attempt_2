d93fb319c820a9ed6eb11f1ef8a0ad775fecf020
Foat <Khisamov.foat@mail.ru>
2023-12-17 12:32:44 +1000

    Семинар 4. Домашнее задание 17.12.2023

---
+++ src/Seminar4/TaskDZ2.java
@@ -0,0 +1,60 @@
+package Seminar4;
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди,
+// dequeue() - возвращает первый элемент из очереди и удаляет его,
+// first() - возвращает первый элемент из очереди, не удаляя.
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TaskDZ2 {
+    // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+    class MyQueueInt {
+        private LinkedList<Integer> elements = new LinkedList<>();
+
+        public void enqueue(int element) {
+            elements.add(element);
+        }
+
+        public int dequeue() {
+            return elements.removeFirst();
+        }
+
+        public int first() {
+            return elements.getFirst();
+        }
+
+        public LinkedList<Integer> getElements() {
+            return elements;
+        }
+    }
+
+    // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+    public class Printer {
+        public void main(String[] args) {
+            MyQueueInt queue;
+            queue = new MyQueueInt();
+
+            if (args.length == 0) {
+                // При отправке кода на Выполнение, вы можете варьировать эти параметры
+                queue.enqueue(1);
+                queue.enqueue(10);
+                queue.enqueue(15);
+                queue.enqueue(5);
+            } else {
+                queue.enqueue(Integer.parseInt(args[0]));
+                queue.enqueue(Integer.parseInt(args[1]));
+                queue.enqueue(Integer.parseInt(args[2]));
+                queue.enqueue(Integer.parseInt(args[3]));
+            }
+
+            System.out.println(queue.getElements());
+
+            queue.dequeue();
+            queue.dequeue();
+            System.out.println(queue.getElements());
+
+            System.out.println(queue.first());
+        }
+    }
+}
